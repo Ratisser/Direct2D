4f5db61be76f052c814a1056a51122073c9eed5f
Ratisser <w_vista@naver.com>
2022-03-21 20:10:45 +0900

    fsm 업데이트중

--- GameEngineBase/GameEngineFSM.cpp
+++ GameEngineBase/GameEngineFSM.cpp
@@ -1,9 +1,31 @@
 #include "PreCompile.h"
 #include "GameEngineFSM.h"
-#include "GameEngineDebug.h"
+
+GameEngineFSM::GameEngineFSM()
+	: current_(nullptr)
+	, next_(nullptr)
+{
+
+}
+
+GameEngineFSM::~GameEngineFSM()
+{
+
+}
 
 
-// Static Var
-// Static Func
+void GameEngineFSM::CreateState(const std::string& _stateName, std::function<void()> _start, std::function<void()> _update, std::function<void()> _init, std::function<void()> _end)
+{
+}
 
-// constructer destructer
+void GameEngineFSM::Update()
+{
+	if (nullptr != next_)
+	{
+		if (nullptr != current_)
+		{
+			current_->end_();
+		}
+
+	}
+}
