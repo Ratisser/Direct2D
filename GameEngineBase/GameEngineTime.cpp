c2d7da7fb5931a7ba870bb29409ec314def75aaf
Ratisser <w_vista@naver.com>
2022-04-17 01:36:24 +0900

    추가 : 타임스케일 추가

--- GameEngineBase/GameEngineTime.cpp
+++ GameEngineBase/GameEngineTime.cpp
@@ -6,18 +6,12 @@ GameEngineTime* GameEngineTime::Inst = new GameEngineTime();
 // Static Func
 // constructer destructer
 GameEngineTime::GameEngineTime()
+	: timeScale_(1.0f)
 {
 	TimeCheckReset();
 }
 
 GameEngineTime::~GameEngineTime()
-{
-}
-
-GameEngineTime::GameEngineTime(const GameEngineTime&& _Other)
-	: timeCount_(_Other.timeCount_),
-	startCheck_(_Other.startCheck_),
-	endCheck_(_Other.endCheck_)
 {
 }
 
@@ -28,6 +22,7 @@ void GameEngineTime::TimeCheckReset()
 	QueryPerformanceFrequency(&timeCount_);
 	QueryPerformanceCounter(&startCheck_);
 	QueryPerformanceCounter(&endCheck_);
+	timeScale_ = 1.0f;
 }
 
 void GameEngineTime::TimeCheck()
@@ -35,4 +30,9 @@ void GameEngineTime::TimeCheck()
 	QueryPerformanceCounter(&endCheck_);
 	deltaTime_ = static_cast<double>((endCheck_.QuadPart - startCheck_.QuadPart)) / static_cast<double>(timeCount_.QuadPart);
 	startCheck_.QuadPart = endCheck_.QuadPart;
+}
+
+void GameEngineTime::SetTimeScale(float _scale)
+{
+	timeScale_ = _scale;
 }
