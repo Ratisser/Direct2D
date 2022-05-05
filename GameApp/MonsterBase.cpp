44787358ff3619c12a9d0c1e838910e41af276bf
Ratisser <w_vista@naver.com>
2022-05-05 23:34:42 +0900

    주석제거

--- GameApp/MonsterBase.cpp
+++ GameApp/MonsterBase.cpp
@@ -18,27 +18,6 @@ MonsterBase::~MonsterBase()
 
 }
 
-//void MonsterBase::Start()
-//{
-//	{
-//		GameEngineRenderer* rc = CreateTransformComponent<GameEngineRenderer>(GetTransform());
-//		rc->SetRenderingPipeline("BoxRendering");
-//		rc->SetScale(100.f);
-//	}
-//
-//	collider_ = CreateTransformComponent<GameEngineCollision>(GetTransform());
-//	collider_->SetCollisionType(eCollisionType::Rect);
-//	collider_->SetCollisionGroup(eCollisionGroup::Monster);
-//	collider_->SetScale(100.f);
-//
-//	GetTransform()->SetLocation(100, 0, 0);
-//	
-//}
-//
-//void MonsterBase::Update(float _deltaTime)
-//{
-//}
-
 void MonsterBase::SetHP(int _hp)
 {
 	hp_ = _hp;
