ff259b8e5e53aa6020d732935d1d167eca98a1bd
Ratisser <w_vista@naver.com>
2022-05-23 21:22:43 +0900

    Add : Add random bool function

--- GameEngineBase/GameEngineRandom.h
+++ GameEngineBase/GameEngineRandom.h
@@ -1,12 +1,29 @@
 #pragma once
 
-// 분류 : 
-// 용도 : 
-// 설명 : 
 class GameEngineRandom
 {
-private:	// member Var
-	std::mt19937_64 mt_;
+public:
+	GameEngineRandom()
+		: mt_()
+	{
+		std::random_device rng_;
+		mt_.seed(rng_());
+	}
+
+	GameEngineRandom(__int64 _Seed)
+		: mt_(_Seed)
+	{
+
+	}
+	~GameEngineRandom()
+	{
+
+	}
+
+	GameEngineRandom(const GameEngineRandom& _other) = delete;
+	GameEngineRandom(GameEngineRandom&& _other) noexcept;
+	GameEngineRandom& operator=(const GameEngineRandom& _other) = delete;
+	GameEngineRandom& operator=(const GameEngineRandom&& _other) = delete;
 
 public:
 	int RandomInt(int _min, int _max)
@@ -21,30 +38,14 @@ public:
 		return Dis(mt_);
 	}
 
-	GameEngineRandom()
-		: mt_()
+	bool RandomBool()
 	{
-		std::random_device rng_;
-		mt_.seed(rng_());
+		std::uniform_int_distribution<int> Dis(0, 1);
+		return static_cast<bool>(Dis(mt_));
 	}
 
-
-	GameEngineRandom(__int64 _Seed)
-		: mt_(_Seed)
-	{
-
-	}
-	~GameEngineRandom()
-	{
-
-	}
-protected:
-	GameEngineRandom(const GameEngineRandom& _other) = delete;
-	GameEngineRandom(GameEngineRandom&& _other) noexcept;
 private:
-	GameEngineRandom& operator=(const GameEngineRandom& _other) = delete;
-	GameEngineRandom& operator=(const GameEngineRandom&& _other) = delete;
+	std::mt19937_64 mt_;
 
-public:
 };
 
