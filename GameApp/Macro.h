589e4c309d5a52c4128401f1d9683e99b6acfb58
Ratisser <w_vista@naver.com>
2022-05-25 21:26:12 +0900

    Add : Add Macro make state with end function

--- GameApp/Macro.h
+++ GameApp/Macro.h
@@ -1,3 +1,8 @@
 #pragma once
 
-#define MakeState(ClassName, StateName) #StateName, std::bind(&ClassName::start##StateName, this, std::placeholders::_1), std::bind(&ClassName::update##StateName, this, std::placeholders::_1)
+#define MakeState(ClassName, StateName) #StateName, std::bind(&ClassName::start##StateName, this, std::placeholders::_1) \
+		, std::bind(&ClassName::update##StateName, this, std::placeholders::_1)
+
+#define MakeStateWithEnd(ClassName, StateName) #StateName, std::bind(&ClassName::start##StateName, this, std::placeholders::_1) \
+		, std::bind(&ClassName::update##StateName, this, std::placeholders::_1) \
+		, std::bind(&ClassName::end##StateName, this, std::placeholders::_1)
